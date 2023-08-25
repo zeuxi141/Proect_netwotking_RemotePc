71c250fde35bf5d3798045d800d388fbb9f5f604
zeuxi141 <74851601+zeuxi141@users.noreply.github.com>
2023-08-25 22:40:32 +0700

    tạo khung cho project

---
+++ src/GUI_test.py
@@ -0,0 +1,10 @@
+from tkinter import *
+
+
+
+Gui_start = Tk() #khởi chạy giao diện
+Gui_start.title('Remote PC') # thiết lập tiêu đề
+Gui_start.geometry('800x600') #thiết lập kích thước màn hình 800x600
+
+
+Gui_start.mainloop()
